431b0f0c0ae13570b244219516e5bdf80ae3dbbe
wutongxue <wuwenqingapple@icloud.com>
2021-01-22 00:36:48 +0800

    create basic route

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    App
+    <hr>
     <router-view/>
+    <hr>
+    <div>
+      <router-link to="/money">Money</router-link>
+      <router-link to="/labels">Labels</router-link>
+      <router-link to="/statistics">Statistics</router-link>
+    </div>
   </div>
 </template>
 
