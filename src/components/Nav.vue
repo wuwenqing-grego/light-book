a2545fbd5966d4dfdff26d999323033b1b9fe743
wutongxue <wuwenqingapple@icloud.com>
2021-01-24 00:54:25 +0800

    add svg-sprite-loader; add config; import svg pictures

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,31 @@
 <template>
     <div class="nav">
-        <router-link to="/money">Money</router-link>
-        <router-link to="/labels">Labels</router-link>
-        <router-link to="/statistics">Statistics</router-link>
+        <router-link to="/money">
+            <svg>
+                <use xlink:href="#gold-coin"/>
+            </svg>
+            Money
+        </router-link>
+        <router-link to="/labels">
+            <svg>
+                <use xlink:href="#label"/>
+            </svg>
+            Labels
+        </router-link>
+        <router-link to="/statistics">
+            <svg>
+                <use xlink:href="#balance-list"/>
+            </svg>
+            Statistics
+        </router-link>
     </div>
 </template>
 
 <script>
+    import '@/assets/icons/label.svg'
+    import '@/assets/icons/gold-coin.svg'
+    import '@/assets/icons/balance-list.svg'
+
     export default {
         
     }
