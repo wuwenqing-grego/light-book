4f51791afa44dda2dc1961d3c86f3701cc0c1bf6
wutongxue <wuwenqingapple@icloud.com>
2021-01-23 21:40:56 +0800

    add 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div>
+            Current page doesn't exist, please check if your path is right!
+        </div>
+        <router-link to="/">Back to homepage</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
