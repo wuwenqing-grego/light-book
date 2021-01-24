eb6cb9f461d6b38f3f5dc0537a783c0cf344b6be
wutongxue <wuwenqingapple@icloud.com>
2021-01-24 23:27:23 +0800

    add icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,25 @@
+<template>
+    <svg class="icon">
+        <use :xlink:href="iconName"/>
+    </svg>
+</template>
+
+<script>
+    import '@/assets/icons/label.svg'
+    import '@/assets/icons/gold-coin.svg'
+    import '@/assets/icons/balance-list.svg'
+
+    export default {
+        props: ['iconName']
+    }
+</script>
+
+<style lang="scss" scoped>
+.icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,31 +1,21 @@
 <template>
     <div class="nav">
         <router-link to="/money">
-            <svg>
-                <use xlink:href="#gold-coin"/>
-            </svg>
+            <Icon icon-name="#gold-coin"/>
             Money
         </router-link>
         <router-link to="/labels">
-            <svg>
-                <use xlink:href="#label"/>
-            </svg>
+            <Icon icon-name="#label"/>
             Labels
         </router-link>
         <router-link to="/statistics">
-            <svg>
-                <use xlink:href="#balance-list"/>
-            </svg>
+            <Icon icon-name="#balance-list"/>
             Statistics
         </router-link>
     </div>
 </template>
 
 <script>
-    import '@/assets/icons/label.svg'
-    import '@/assets/icons/gold-coin.svg'
-    import '@/assets/icons/balance-list.svg'
-
     export default {
         
     }
