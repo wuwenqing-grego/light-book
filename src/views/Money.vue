21efb41b47c927c099e94e4bb2a38e7e8d86585f
wutongxue <wuwenqingapple@icloud.com>
2021-01-27 00:21:05 +0800

    add Money tags style

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -4,13 +4,17 @@
             <p>Money</p>
 
             <div class="tags">
-                <ul>
+                <ul class="list">
+                    <li>衣</li>
+                    <li>食</li>
+                    <li>住</li>
+                    <li>行</li>
                     <li>衣</li>
                     <li>食</li>
                     <li>住</li>
                     <li>行</li>
                 </ul>
-                <div>
+                <div class="new">
                     <button>新增标签</button>
                 </div>
             </div>
@@ -51,3 +55,37 @@
         </Layout>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.tags {
+    font-size: 14px;
+    padding: 16px;
+
+    .list {
+        display: flex;
+        flex-wrap: wrap;
+
+        li {
+            background: #d9d9d9;
+            height: 24px;
+            line-height: 24px;
+            border-radius: 12px;
+            padding: 0 16px;
+            margin-right: 12px;
+            margin-top: 4px;
+        }
+    }
+
+    .new {
+        padding-top: 16px;
+
+        button {
+            background: transparent;
+            border: none;
+            color: #999;
+            border-bottom: 1px solid;
+            padding: 0 4px;
+        }
+    }
+}
+</style>
