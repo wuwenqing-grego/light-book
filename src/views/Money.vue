afef2915ff853e6192816d399eedd5bd4763e266
wutongxue <wuwenqingapple@icloud.com>
2021-01-26 23:53:17 +0800

    add Money html; add global style;

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,6 +2,52 @@
     <div>
         <Layout>
             <p>Money</p>
+
+            <div class="tags">
+                <ul>
+                    <li>衣</li>
+                    <li>食</li>
+                    <li>住</li>
+                    <li>行</li>
+                </ul>
+                <div>
+                    <button>新增标签</button>
+                </div>
+            </div>
+            <div class="notes">
+                <label>
+                    <span>备注</span>
+                    <input type="text" placeholder="在这里添加备注">
+                </label>
+            </div>
+            <div class="types">
+                <ul>
+                    <li class="selected">支出</li>
+                    <li>收入</li>
+                </ul>
+            </div>
+            <div class="numPanel">
+                <div class="output">100</div>
+                <div class="buttons">
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>+</button>
+                    <button>back</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>-</button>
+                    <button>C</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>*</button>
+                    <button>OK</button>
+                    <button>0</button>
+                    <button>.</button>
+                </div>
+            </div>
         </Layout>
     </div>
 </template>
