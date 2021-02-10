3094481927d233607bc3315986c273812c232642
wutongxue <wuwenqingapple@icloud.com>
2021-02-10 21:32:54 +0800

    change Labels li to router-link; add EditLabel created function

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,12 +1,29 @@
 <template>
     <Layout>
         editlabel
+        {{tag}}
     </Layout>
 </template>
 
-<script>
-    export default {
+<script lang="ts">
+    import { Vue, Component } from 'vue-property-decorator'
+    import tagListModel from '@/models/tagListModel.ts'
+
+    @Component
+    export default class EditLabel extends Vue {
+        tag?: { id: string; name: string } = undefined
         
+        created() {
+            const id = this.$route.params.id
+            const tags = tagListModel.fetch()
+            const currTag = tags.filter(item => item.id === id)[0]
+
+            if (currTag) {
+                this.tag = currTag
+            } else {
+                this.$router.replace('/404')
+            }
+        }
     }
 </script>
 
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,11 +1,11 @@
 <template>
     <Layout>
-        <ul class="tags">
-            <li class="tag" v-for="tag in tags" :key="tag.id">
+        <div class="tags">
+            <router-link class="tag" v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${tag.id}`">
                 <span>{{tag.name}}</span>
                 <Icon icon-name="#arrow"/>
-            </li>
-        </ul>
+            </router-link>
+        </div>
         <div class="newTag-wrapper">
             <button class="newTag" @click="createTag">新建标签</button>
         </div>
