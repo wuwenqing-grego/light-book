66b99f28e078283e75ab518fc3a3c348c02a42dc
wutongxue <wuwenqingapple@icloud.com>
2021-02-20 23:05:23 +0800

    add Tabs component as super class of Types; use Tabs in Statistics

---
+++ src/components/Money/Tabs.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <ul class="tabs" :class="{[classPrefix + '-tabs']: classPrefix}">
+            <li v-for="tab in tabList" :key="tab.value" class="tabs-item"
+            :class="tabClass(tab)" @click="select(tab)">
+                {{tab.text}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Vue, Component, Prop } from 'vue-property-decorator'
+
+    type TabItem = { text: string; value: string }
+
+    @Component
+    export default class Types extends Vue {
+        @Prop({required: true, type: Array}) tabList!: TabItem[]
+        @Prop(String) readonly value!: string
+        @Prop(String) classPrefix?: string
+
+        select(tab: TabItem) {
+            this.$emit('update:value', tab.value)
+        }
+
+        tabClass(tab: TabItem) {
+            return {
+                selected: tab.value === this.value,
+                [this.classPrefix + '-tabs-item']: this.classPrefix
+            }
+        }
+    }
+
+</script>
+
+<style lang="scss" scoped>
+.tabs {
+    background: #c4c4c4;
+    display: flex;
+    font-size: 24px;
+
+    li {
+        width: 50%;
+        height: 64px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+
+        &.selected::after {
+            content: '';
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 4px;
+            background: #333;
+        }
+    }
+}
+</style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,7 +1,42 @@
 <template>
-    <div>
-        <Layout>
-            <p>Statistics</p>
-        </Layout>
-    </div>
+    <Layout>
+        <Tabs class-prefix="type" :tab-list="recordTypeList" :value.sync="type"/>
+        <Tabs class-prefix="interval" :tab-list="intervalList" :value.sync="interval"/>
+    </Layout>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Tabs from '@/components/Money/Tabs.vue'
+
+@Component({
+    components: { Tabs }
+})
+export default class Statistics extends Vue {
+    type = '-'
+    recordTypeList = [
+        {text: '支出', value: '-'},
+        {text: '收入', value: '+'},
+    ]
+    interval = 'day'
+    intervalList = [
+        {text: '按天', value: 'day'},
+        {text: '按周', value: 'week'},
+        {text: '按月', value: 'month'},
+    ]
+}
+</script>
+
+<style lang="scss" scoped>
+::v-deep {
+    .type-tabs-item {
+        &.selected {
+            background: white;
+        }
+    }
+
+    li.interval-tabs-item {
+        height: 48px;
+    }
+}
+</style>
