c3fd60f613e84aed6ed061bcc96673a2d0236fce
wutongxue <wuwenqingapple@icloud.com>
2021-02-25 20:57:48 +0800

    modify config for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/light-book-dist/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/light-book/' : '/',
 
   lintOnSave: false,
 
