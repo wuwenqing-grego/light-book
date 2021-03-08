f32fd0e018018485e059cdb38272ad6c553b7117
wutongxue <wuwenqingapple@icloud.com>
2021-03-08 22:35:03 +0800

    deploy

---
+++ precache-manifest.be4132861754070ffa966852487c5ddc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f76bfceaee3248c258d7",
+    "url": "/light-book/css/app.4800fbda.css"
+  },
+  {
+    "revision": "1dc6a0a75f6a357369290599aa43509a",
+    "url": "/light-book/index.html"
+  },
+  {
+    "revision": "f76bfceaee3248c258d7",
+    "url": "/light-book/js/app.10d0bed7.js"
+  },
+  {
+    "revision": "6c8f0962222f60b40d82",
+    "url": "/light-book/js/chunk-vendors.36cb87fd.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/light-book/manifest.json"
+  },
+  {
+    "revision": "51d80bc214187270862f1b2a069223c2",
+    "url": "/light-book/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/light-book/robots.txt"
+  }
+]);
